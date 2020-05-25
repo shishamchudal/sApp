f2523ae856868f5e3da7e35eab1c5678bebc6ba4
Shisham Chudal <ShishamChudal61@gamil.com>
2020-05-25 16:45:06 +0545

    Fixed Header

--- includes/search.php
+++ includes/search.php
@@ -1,7 +1,7 @@
 <!-- Topbar Search -->
 
 
-    <div class="input-group">
+<div class="input-group" style="width: 50%;">
           <form method="post" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group">
               <input type="text" class="form-control bg-light border-0 small" placeholder="Search for Customers..." aria-label="Search" aria-describedby="basic-addon2" name="search">
