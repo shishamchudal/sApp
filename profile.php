126d800ae3c396dcf31dc8d91806472bf0508ee4
Shisham Chudal <ShishamChudal61@gamil.com>
2020-05-25 20:48:29 +0545

    Profile page showing everything

--- profile.php
+++ profile.php
@@ -1,25 +1,4 @@
-<?php
 
-
-// We need to use sessions, so you should always start sessions using the below code.
-
-
-include 'cred_account.php';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	die('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email, User_type FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->bind_result($password, $email, $User_type);
-$stmt->fetch();
-$stmt->close();
-
-
-?>
 <?php 
 
 include 'includes/header.php'; 
@@ -62,7 +41,28 @@ include('database_connection.php');
 
 				<?php include("includes/topbar.php"); ?>
 
+<?php
 
+
+// We need to use sessions, so you should always start sessions using the below code.
+
+
+include 'cred_account.php';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// We don't have the password or email info stored in sessions so instead we can get the results from the database.
+$stmt = $con->prepare('SELECT password, email, User_type FROM accounts WHERE id = ?');
+// In this case we can use the account ID to get the account info.
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email, $User_type);
+$stmt->fetch();
+$stmt->close();
+
+
+?>
 
 				<div class="container-fluid">
 					<h2> Profile Page</h2>
